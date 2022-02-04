0a00906e603c26fd65b4d41bbc1c07bddf75a20c
Admin <Admin@DESKTOP-7752MH0>
2022-02-04 20:49:47 +0530

    add

---
+++ demo1/src/pack/class1.java
@@ -0,0 +1,14 @@
+package pack;
+
+public class class1 {
+	public static void main(String[]args)
+	{
+		System.out.println("hi");
+	 
+		System.out.println("hello");
+		System.out.println("new");
+		
+	}
+  
+	
+}
